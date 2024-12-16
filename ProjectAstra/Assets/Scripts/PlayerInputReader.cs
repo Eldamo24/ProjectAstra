4c0a0e8c25aefc636b5b18d4778db194b6882f64
Damian Lobos <damianlobos24@gmail.com>
2024-12-16 10:18:07 -0300

    Salto

---
+++ ProjectAstra/Assets/Scripts/PlayerInputReader.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputReader : MonoBehaviour
+{
+    [SerializeField] private PlayerInput playerInput;
+    private Vector3 movementVector;
+    private bool jumping;
+    public Vector3 MovementVector { get => movementVector; set => movementVector = value; }
+    public bool Jumping { get => jumping; set => jumping = value; }
+
+    private void Start()
+    {
+        jumping = false;
+    }
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        movementVector = playerInput.actions["Move"].ReadValue<Vector3>();
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            jumping = true;
+        }
+        else if (context.canceled)
+        {
+            jumping = false;
+        }
+    }
+}
--- ProjectAstra/Assets/Scripts/TPController.cs
+++ ProjectAstra/Assets/Scripts/TPController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class TPController : MonoBehaviour
@@ -7,9 +8,21 @@ public class TPController : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private Transform playerObj;
     [SerializeField] private Rigidbody rb;
+    [SerializeField] private PlayerInputReader playerInputReader;
 
+    private Vector3 inputs;
+
+    [Header("Speed Variables")]
     [SerializeField] private float rotationSpeed;
     [SerializeField] private float speedMovement;
+
+    [Header("Jump References")]
+    [SerializeField] private float jumpForce;
+    [SerializeField] private bool isGrounded;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private Transform checkGround;
+
+
 
     private void Start()
     {
@@ -19,15 +32,36 @@ public class TPController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        Movement();
+        Grounded();
+        if (playerInputReader.Jumping && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+
+
+    private void Movement()
+    {
         Vector3 viewDir = player.position - new Vector3(Camera.main.transform.position.x, player.position.y, Camera.main.transform.position.z);
         orientation.forward = viewDir.normalized;
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 inputDir = orientation.forward * vertical + orientation.right * horizontal;
-        if(inputDir != Vector3.zero)
+        inputs = playerInputReader.MovementVector;
+        Vector3 inputDir = orientation.forward * inputs.z + orientation.right * inputs.x;
+        if (inputDir != Vector3.zero)
         {
             playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
             rb.MovePosition(rb.position + inputDir * speedMovement * Time.deltaTime);
         }
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce);
+    }
+
+    private void Grounded()
+    {
+        isGrounded = Physics.CheckSphere(checkGround.position, 0.2f, groundLayer);
     }
 }
